fda72f51fd900dbafaa8a0f306019078edd2bae8
Denis Popov <d.popov93@mail.ru>
2021-01-12 02:52:09 +0500

    Update version, added documentation

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # conflibsaver
 Library for easy working with configuration files
+
+[![License GPL](https://img.shields.io/badge/license-GPL-blue.svg)](LICENSE)
+
+### Lib functions 🔧
+| Name                | Arguments               | Description                                                     | Return type    |
+| ------------------- | ----------------------- | --------------------------------------------------------------- | -------------- |
+| read_file           |                         | Reads file, recording to self.data_string                       | Boolean        |
+| create_file         |                         | Create configuration file                                       | Boolean        |
+| create_path_to_file |                         | Create path to file                                             | Boolean        |
+| install_file_path   | conf_file_path          | Validate and saves to configuration file name and path of file  | String         |
+| param_count         |                         | Return count of parameters in config file                       | Integer        |
+| set_param           | param_name, param_value | Set parameter value by name                                     | Boolean        |
+| get_param           | param_name              | Return value of parameter by name                               | String or None |
+| has_param           | param_name              | Check for an existing parameter                                 | Boolean        |
+| save_data           |                         | Saves data to file                                              | Boolean        |
+| data_to_string      |                         | Converts data in one line (syntax conflib file)                 | String         |
+| get_filename        |                         | Returns name of config file                                     | String         |
+| get_path            |                         | Returns path of config file                                     | Boolean        |
+| dbg                 | text                    | Display output of errors if display messages is on              |                |
+
+### Thank You!
+Please ⭐️ this repo and share it with others
+
+### Syntax 🔌
+For include library to your project you need to import lib:
+
+       from conflibsaver import conflibsaver
+Create settings instance:
+
+       settings = conflibsaver("~/.config/myapplication/settings.cfg")
+Create or rewrite parameter "param1" with value "some_value":
+
+       settings.set_param("param1", "some_value")
+Get value of parameter "param1":
+
+       if settings.has_param("param1"):
+           print(settings.get_param("param1"))
+To get count of parameters use param_count():
+
+       print(settings.param_count())
+Printing name of file and path of filename:
+
+       print("file:", settings.get_filename())
+       print("dir:", settings.get_path())
+Data save when you close the application, but you may get this manually:
+
+       settings.save_data()
+
+### Attention
+* Please don't use in parameter names or value whitespaces or special symbol '='
+
+### Contributing 💡
+If you want to contribute to this project and make it better with new ideas, your pull request is very welcomed.
+If you find any issue just put it in the repository issue section, thank you.
--- conflibsaver.py
+++ conflibsaver.py
@@ -7,120 +7,231 @@ edit and using library for your program
 setting manipulation
 
 Author: Denis Popov
-E-mail: spidermind93@gmail.com
+E-mail: d.popov93@mail.ru
 Created: 30.11.2017
-Version: 0.1a
+Version: 0.2
 
 '''
 
 import os
+from pathlib import Path
 
-def str2bool(string) :
-	try :
-		if string.lower() == 'true' : return True
-		elif string.lower() == 'false' : return False
-	except AttributeError : 
-		return None
-
-def create_file(path_to_file, string_config = 'None', display_messages = False) :
-	try :
-		with open(path_to_file, 'w') as f:
-			f.write(string_config)
-	except IOError :
-		if display_messages == True : print('IOError: Perhaps path to file is not found. Try to create...')
-		create_path_to_file(path_to_file)
-
-def create_path_to_file(path_to_file, string_config = 'None', display_messages = False) :
-	tmp_addr = ''
-	for dirs in os.path.realpath(path_to_file).split('/') [1:-1] :
-		tmp_addr += '/' + dirs
-		if os.path.exists(tmp_addr) != True :
-			try :
-				os.makedirs(tmp_addr)
-			except FileExistsError :
-				if display_messages == True : print('Error: directory is exists')
-			create_file(path_to_file, string_config)
-
-def read_file(path_to_file, string_config = 'None', display_messages = False) :
-	data = None
-	try :
-		with open(path_to_file, 'r') as f :
-			data = f.read().split()
-	except FileNotFoundError :
-		if display_messages == True :
-			while True :
-				answer = input ('FileNotFoundError: configuration file is not exists. Create new file? (y / n): ')
-				if answer.lower() not in ('y', 'n', 'yes', 'no') :
-					print('Answer is wrong... Try "y" or "n"')
-				else :
-					if answer.lower() in ('y', 'yes') : create_file(path_to_file, string_config)
-	else :
-		if len(data) < 3 : data = ['None']
-	finally :
-		return data
-
-def param_count(path_to_file) :
-	try :
-		return len(read_file(path_to_file)) // 3
-	except ZeroDivisionError :
-		print('Error: file is epmty!')
-		return 0
-	except :
-		print('Error reading file')
-		return 0
-
-def data_to_string(data) :
-	complete_string = ''
+class conflibsaver:
+	def __init__(self, conf_file_path, display_messages=False):
+		### Main variables ###
+		self.data_string = None
+		self.data = None
+		self.conf_file_name = None
+		self.conf_path = None
+		self.display_messages = display_messages
+		self.conf_file_path = self.install_file_path(conf_file_path)
+		
+		if os.path.exists(self.conf_file_path):
+			self.read_file()
+		else:
+			self.create_file()
 	
-	for x in range (0, len(data) // 3) :
-		for y in range (0, 3) :
-			complete_string += data[x*3+y] + ' '
-		complete_string += '\n'
+	def __del__(self):
+		"""Destructor save data to config file"""
+		self.save_data()
+				
+			
 	
-	return complete_string
+	def read_file(self):
+		"""Reads file, recording to self.data_string.
+		
+		Returns:
+		True -- successful reading configuration file
+		False -- unsuccessful file read
+		
+		"""
+		try:
+			with open(self.conf_file_path, 'r') as f:
+				self.data_string = f.read().split()
+				self.data = {
+					string.split('=')[0]: string.split('=')[1]
+					for string in self.data_string
+				}
+		except IndexError as e:
+			self.dbg(str(e) + ": Can\'t to parse config file. Created new")
+			self.create_file()
+			return False
+		except FileNotFoundError as e:
+			self.dbg(str(e) + ": Config file not found. Created new")
+			self.create_file()
+			return False
+		else:
+			self.dbg("Parameters in file not founded")
+			if len(self.data_string) < 1: return False
+		finally:
+			return True
+	
+	def create_file(self):
+		"""Create configuration file
+		
+		Returns:
+		True -- successful creating configuration file
+		False -- unable to create configuration file
+		
+		"""
+		try:
+			open(self.conf_file_path, 'w').close()
+			return True
+		except FileNotFoundError:
+			return self.create_path_to_file()
+		except PermissionError as e:
+			self.dbg(str(e) + ": unable to create config file, need root permissions")
+			return False
 
-def set_param(path_to_file, param_name, param_value) :
-	param_is_exists = has_param(path_to_file, param_name)
-	if param_is_exists == None : return None
-	else :
-		data = read_file(path_to_file)
-	
-		if data == None :
-			data = [param_name, '=', param_value]
-		else :
-			if len(data) >= 3 :
-				if param_is_exists :
-					for index, item in enumerate(data) :
-						if item == param_name :
-							data[index+2] = param_value
-				else :
-					data += [param_name, '=', param_value]
-			else :
-				data = [param_name, '=', param_value]
-	
-		try :
-			with open(path_to_file, 'w') as f :
-				f.write(data_to_string(data))
-		except IOError :
-			print('IOError: error adding param to file')
+	def create_path_to_file(self):
+		"""Create path to file
+		
+		Returns:
+		True -- successful creating dir
+		False -- unable to create dir
+		
+		"""
+		try:
+			os.makedirs(Path(self.conf_file_path).parent)
+			return self.create_file()
+		except FileExistsError as e:
+			self.dbg(str(e) + ": unable to create config dir. Please, validate folder path")
 			return False
 	
-		return True
+	def install_file_path(self, conf_file_path):
+		"""Validate and saves to configuration file name and path of file
+		
+		Keyword arguments:
+		conf_file_path -- string of file location
+		
+		Returns:
+		Valid string of file path
+		"""
+		conf_file_path = os.path.expanduser(conf_file_path)
+		
+		self.conf_file_name = Path(conf_file_path).name
+		self.conf_path = Path(conf_file_path).parent
+		
+		return conf_file_path
 
-def get_param(path_to_file, param_name) :
-	data = read_file(path_to_file)
+	def param_count(self):
+		"""Return count of parameters in config file
+		
+		Returns:
+		0 -- if parameters in file not founded
+		n > 0 -- parameter count
+		"""
+		if self.data != None:
+			return len(self.data)
+		else:
+			return 0
 	
-	try :
-		for index, item in enumerate(data) :
-			if item == param_name : return data[index+2]
-	except TypeError :
-		print('TypeError: data is empty')
+	def set_param(self, param_name, param_value):
+		"""Set parameter value by name
+		
+		Keyword arguments:
+		param_name -- Parameter name
+		param_value -- Parameter value
+		
+		Returns:
+		True -- successful parameter setting
+		False -- unsuccessful parameter setting
+		"""
+		try:
+			self.data[str(param_name)] = str(param_value)
+			return True
+		except:
+			return False
+			
+	
+	def get_param(self, param_name):
+		"""Return value of parameter by name
+		
+		Keyword arguments:
+		param_name -- parameter's name in string
+		
+		Returns:
+		Value -- if parameter by name is exists
+		None -- if parameter is not founded
+		"""
+		try:
+			return self.data[param_name]
+		except KeyError as e:
+			self.dbg(str(e) + ": Param \"" + param_name + "\" is not founded")
+			return None
+		except NoneType as e:
+			self.dbg(str(e) + ": Param \"" + param_name + "\" is not founded")
+			return None
+	
+	def has_param(self, param_name):
+		"""Check for an existing parameter
+		
+		Keyword arguments:
+		param_name -- parameter's name in string
+		
+		Returns:
+		True -- if parameter by name is exists
+		False -- if parameter is not founded
+		"""
+		if param_name in self.data:
+			return True
+		else:
+			return False
+	
+	def save_data(self):
+		"""Saves data to file
+		
+		Returns:
+		True -- writing is successful
+		False -- data is not writted
+		"""
+		if self.param_count() == False:
+			self.dbg("Nothing to writing")
+			return False
+			
+		try:
+			with open(self.conf_file_path, 'w') as f:
+				f.write(self.data_to_string(self.data))
+		except FileNotFoundError as e:
+			self.dbg(str(e) + ": unable to write data to file")
+			return False
+		except PermissionError as e:
+			self.dbg(str(e) + ": unable to write data to file, need root permissions")
+			return False
+		else:
+			return True
+	
+	def data_to_string(self, data):
+		"""Converts data in one line (syntax conflib file)
+		
+		Keyword argument:
+		data -- dictionary of params with values
+		
+		Returns:
+		String -- converted dictionary to one line
+		"""
+		return '\n'.join([f'{key}={value}' for key, value in data.items()])
+	
+	def get_filename(self):
+		"""Returns name of config file
+		
+		Returns:
+		String -- name of config file
+		"""
+		return self.conf_file_name
+	
+	def get_path(self):
+		"""Returns path of config file
+		
+		Returns:
+		String -- name of config file's path
+		"""
+		return self.conf_path
 
-def has_param(path_to_file, param_name) :
-	data = read_file(path_to_file)
-	
-	if data != None and len(data) >= 3 :
-		for item in data :
-			if item == param_name : return True
-		return False
-	else : return None
+	def dbg(self, text=""):
+		"""Display output of errors if display messages is on
+		
+		Keyword arguments:
+		text -- string message for display
+		"""
+		if self.display_messages: print(text)
